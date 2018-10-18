35fb9327152b2e756a3451004c703ff55760bf9b
Tyler <tyler.roberts93@outlook.comgit config --global user.email tyler.roberts93@outlook.com>
2018-10-17 20:25:37 -0500

    Got google maps set up

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { load_google_maps } from './utility.js'
 
 class App extends Component {
+
+  componentDidMount() {
+    let googleMaps = load_google_maps();
+
+    Promise.all([
+      googleMaps
+    ]).then(values => {
+      console.log(values);
+    })
+
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div id="google-map">
+
       </div>
     );
   }
---
+++ src/utility.js
@@ -0,0 +1,17 @@
+export function load_google_maps() {
+  return new Promise(function(resolve, reject) {
+    // define the global callback that will run when google maps is loaded
+    window.resolveGoogleMapsPromise = function() {
+      // resolve the google object
+      resolve(window.google);
+      // delete the global callback to tidy up since it is no longer needed
+      delete window.resolveGoogleMapsPromise;
+    }
+    // Now, Load the Google Maps API
+    const script = document.createElement("script");
+    const API_KEY = 'AIzaSyDHiS5vk2_PJXeXXf_-n9gGqlciidm1gZE';
+    script.src = `https://maps.googleapis.com/maps/api/js?libraries=places&key=${API_KEY}&callback=resolveGoogleMapsPromise`;
+    script.async = true;
+    document.body.appendChild(script);
+  });
+}
