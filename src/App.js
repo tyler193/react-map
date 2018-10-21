e13ede6f853850867c6611bd8835f96355016207
Tyler <tyler.roberts93@outlook.comgit config --global user.email tyler.roberts93@outlook.com>
2018-10-20 21:59:01 -0500

    Map renders and markers are on venues

--- src/App.js
+++ src/App.js
@@ -1,17 +1,44 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
-import { load_google_maps } from './utility.js'
+import { load_google_maps, places } from './utility.js'
 
 class App extends Component {
 
   componentDidMount() {
     let googleMaps = load_google_maps();
+    let loadPlaces = places();
 
+    //Load map and venues
     Promise.all([
-      googleMaps
+      googleMaps,
+      loadPlaces
     ]).then(values => {
-      console.log(values);
+      let google = values[0];
+      let venues = values[1].response.venues;
+      this.google = google;
+      this.markers = [];
+
+      //Render google map
+      this.map = new google.maps.Map(document.getElementById('google-map'), {
+        zoom: 15,
+        scrollwheel: true,
+        center: { lat: venues[0].location.lat, lng: venues[0].location.lng }
+      });
+
+      this.infowindow = new google.maps.InfoWindow();
+
+      //Place marker at each venue
+      venues.forEach(venue => {
+        let marker = new google.maps.Marker({
+          position: { lat: venue.location.lat, lng: venue.location.lng },
+          map: this.map,
+          venue: venue,
+          id: venue.id,
+          name: venue.name,
+          animation: google.maps.Animation.DROP
+        });
+      });
     })
 
   }
--- src/utility.js
+++ src/utility.js
@@ -1,3 +1,8 @@
+//Utility file to store all APIs and miscellaneous functions used
+//throughout the application
+
+
+//Loading Google Maps - Thanks to Ryan Waite :)
 export function load_google_maps() {
   return new Promise(function(resolve, reject) {
     // define the global callback that will run when google maps is loaded
@@ -7,11 +12,21 @@ export function load_google_maps() {
       // delete the global callback to tidy up since it is no longer needed
       delete window.resolveGoogleMapsPromise;
     }
-    // Now, Load the Google Maps API
+    //Load the Google Maps API
     const script = document.createElement("script");
     const API_KEY = 'AIzaSyDHiS5vk2_PJXeXXf_-n9gGqlciidm1gZE';
     script.src = `https://maps.googleapis.com/maps/api/js?libraries=places&key=${API_KEY}&callback=resolveGoogleMapsPromise`;
     script.async = true;
     document.body.appendChild(script);
   });
+}
+
+
+//Loads venues with Foursquare API
+export function places() {
+  let city = 'Nashwauk, MN';
+  let query = '';
+  var apiURL = 'https://api.foursquare.com/v2/venues/search?client_id=KHLLC124YG1RZWMKVV1TZ0RW3GX4S1R4NL1ODFN3S5P5XZBZ&client_secret=Z3FSKLIMJTIGWNRJZRZPARHBQ1IM4SWEOCWOOG2QQTF51JGO&v=20130815%20&limit=50&near=' + city + '&query=' + query + '';
+  return fetch(apiURL)
+          .then(resp => resp.json())
 }
