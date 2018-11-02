d91d2a1df16421ff7cfcd268524a4bfa7b6c7b31
Tyler <tyler.roberts93@outlook.comgit config --global user.email tyler.roberts93@outlook.com>
2018-11-01 19:17:23 -0500

    Added a line to <script> for errors on map

--- src/utility.js
+++ src/utility.js
@@ -4,6 +4,7 @@
 
 //Loading Google Maps - Thanks to Ryan Waite :)
 export function load_google_maps() {
+  
   return new Promise(function(resolve, reject) {
     // define the global callback that will run when google maps is loaded
     window.resolveGoogleMapsPromise = function() {
@@ -12,10 +13,13 @@ export function load_google_maps() {
       // delete the global callback to tidy up since it is no longer needed
       delete window.resolveGoogleMapsPromise;
     }
+
+
     //Load the Google Maps API
     const script = document.createElement("script");
     const API_KEY = 'AIzaSyDHiS5vk2_PJXeXXf_-n9gGqlciidm1gZE';
     script.src = `https://maps.googleapis.com/maps/api/js?libraries=places&key=${API_KEY}&callback=resolveGoogleMapsPromise`;
+    script.onerror = `googleError()`;
     script.async = true;
     document.body.appendChild(script);
   });
